edcfdfa221f82f5760b9bcf1af297a265ce84df1
Alif Satria Adhi <alifsatriaadhi@gmail.com>
2025-12-09 16:12:23 +0700

    initial commit

--- services/validateService.js
+++ services/validateService.js
@@ -1,5 +1,7 @@
 export function isValidUsername(input) {
-    const regex = /^([A-Za-z0-9]+)+$/;
+   // const regex = /^([A-Za-z0-9]+)+$/;
+    const regex = /^[A-Za-z0-9]{3,20}$/;
+
 
     return regex.test(input);
 }
